c94ace8f5bc4641dc79285eee21e0e51fd5a6d9b
Scott Sayles <scott.sayles@gmail.com>
2010-12-09 10:41:59 -0500

    adding better handling for how to name the log file and detecting the Rails environment and availability of Logger

--- lib/logit.rb
+++ lib/logit.rb
@@ -6,6 +6,12 @@ module Logit
   module ClassMethods
     DEFAULT_OPTS = {:write_mode => 'a'}
 
+    begin
+      Module.const_get(:Logger)
+    rescue NameError
+      require 'logger'
+    end
+
     # === Options
     # * +:write_mode+ - mode used when opening the log file.  You'll ususually just want 'a' for append or 'w' for overwrite.  Defaults to 'a'.
     # * +:shift_age+ - Number of old logs to keep or frequency of rotation.
@@ -37,18 +43,54 @@ module Logit
     #  
     def logs_to(name, opts={})
       opts = DEFAULT_OPTS.merge(opts)
+      path = logit_log_name(name, opts)
       self.send :define_method, :logger do
         unless @logger
-          path = "#{name}.log"
-          if (Rails)
-            path = File.join(RAILS_ROOT, 'log', "#{name}_#{Rails.env}.log")
-          end
           @logger =  Logit::Logger.new(path, opts)
           if opts[:progname]
             @logger.progname = opts[:progname]
           end
         end
         @logger
+      end
+    end
+
+
+    # Tries to figure out what the fully qualified path name
+    # of the log file should be.
+    #
+    def logit_log_name(name, opts)
+      path = name.to_s.strip
+
+      #  if they are giving a path like '/var/log/foo.log'
+      #  then we shouldn't presume to stick it in the Rails log dir
+      unless (path =~ /\/+/) 
+        if (logit_in_rails?)
+          # take off any trailing .log so we can attach the environment
+          # name
+          path = logit_strip_dot_log(path)
+          path = File.join(RAILS_ROOT, 'log', "#{name}_#{Rails.env}.log")
+        end
+      end
+      # see if we need to append .log
+      # is this a bit presumptuous?
+      unless (path =~ /\.log$/)
+        path << ".log"
+      end
+    end
+
+    def logit_strip_dot_log(name)
+      if (name =~ /\.log$/)
+        name.slice(0, name =~ /\.log$/) 
+      else
+        name
+      end
+    end
+    def logit_in_rails?
+      begin
+        Module.const_get(:Rails)
+        return true
+      rescue NameError
       end
     end
   end
